4eee77343b2d6e76d1f6ac71afd7d433d8c76c77
Andrew S <andrewsheamls@gmail.com>
2022-02-15 16:08:45 -0700

    added an alert to let the user know if they answered correctly or not

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ var ansThree = document.getElementById("answerThree");
 var ansFour = document.getElementById("answerFour");
 var quizzer = document.getElementById("quiz");
 var gameScore = document.getElementById("gameScore");
+var showAns = document.getElementById("showAnswer");
+var finalScore = document.getElementById("finScore");
 
 var questionCount = 0;
 var userScore = 0;
@@ -44,18 +46,18 @@ ansTwo.addEventListener("click", askQuestion);
 ansThree.addEventListener("click", askQuestion);
 ansFour.addEventListener("click", askQuestion);
 
-
-var timeLeft = 60;
+var timeLeft = 5;
 //This function starts the countdown
 function startTimer() {
-//   askQuestion();
+  //   askQuestion();
   var timer = setInterval(function () {
     timeLeft--;
     quizzer.textContent = timeLeft + " seconds remaining";
 
     if (timeLeft === 0) {
       clearInterval(timer);
-      window.alert("Times Up!")
+      window.alert("Times Up!");
+      //call a function here that allows user to enter initals and see the score
     }
   }, 1000);
   question.textContent = questions[questionCount].quest;
@@ -63,32 +65,34 @@ function startTimer() {
   ansTwo.textContent = questions[questionCount].incans[0];
   ansThree.textContent = questions[questionCount].incans[1];
   ansFour.textContent = questions[questionCount].incans[2];
-
-};
-
+}
 
 function askQuestion(event) {
-    console.log(event)
-    var userAnswer = event.target.id;
-    if (userAnswer === questions[questionCount].answer){
-        //handle correct answer
-        userScore += 1;
-        gameScore.textContent = userScore + "/" + questions.length;
-    } else {
-        gameScore.textContent = userScore + "/" + questions.length;
+  console.log(event);
+  var userAnswer = event.target.id;
 
-        // wrong answer
-    }
+  if (userAnswer === questions[questionCount].answer) {
+    //handle correct answer
+    userScore += 1;
+    gameScore.textContent = userScore + "/" + questions.length;
+    // want to notify user answer was correct somehow
+    showAns.textContent = "Correct answer!";
+  } else {
+    gameScore.textContent = userScore + "/" + questions.length;
+    showAns.textContent = "Incorrect answer!";
 
-   if(questionCount < questions.length){
-       questionCount += 1;
-   };
+    // wrong answer
+  }
 
-    console.log(userAnswer === questions[questionCount].answer);
-    question.textContent = questions[questionCount].quest;
-  ansOne.textContent = questions[questionCount].answer;
-  ansTwo.textContent = questions[questionCount].incans[0];
-  ansThree.textContent = questions[questionCount].incans[1];
-  ansFour.textContent = questions[questionCount].incans[2];
-}; 
-   
+  if (questionCount < questions.length) {
+    questionCount += 1;
+  }
+}
+
+// console.log(userAnswer === questions[questionCount].answer);
+// question.textContent = questions[questionCount].quest;
+// ansOne.textContent = questions[questionCount].answer;
+// ansTwo.textContent = questions[questionCount].incans[0];
+// ansThree.textContent = questions[questionCount].incans[1];
+// ansFour.textContent = questions[questionCount].incans[2];
+// };
