ad1d10b9a349c826a8052c0d67ebd8cc16053dc7
Andrew S <andrewsheamls@gmail.com>
2022-02-14 22:52:25 -0700

    added a function to display the questions and save your answer

--- script.js
+++ script.js
@@ -1,59 +1,94 @@
 var question = document.getElementById("quizQuest");
-
 var ansOne = document.getElementById("answerOne");
-
 var ansTwo = document.getElementById("answerTwo");
-
 var ansThree = document.getElementById("answerThree");
-
 var ansFour = document.getElementById("answerFour");
-
 var quizzer = document.getElementById("quiz");
+var gameScore = document.getElementById("gameScore");
 
-var i = 0
+var questionCount = 0;
+var userScore = 0;
 
+// Below is the array of objects containing the questions and answers
 var questions = [
   {
     quest: "Here is question 1?",
-    answer: "correct answer 1",
+    answer: "answerOne",
     incans: ["incorrect answer 1", "incorrect answer 2", "incorrect answer 3"],
   },
   {
     quest: "Here is question 2?",
-    answer: "correct answer 2",
+    answer: "answerOne",
     incans: ["incorrect answer 1", "incorrect answer 2", "incorrect answer 3"],
   },
   {
     quest: "Here is question 3?",
-    answer: "correct answer 3",
+    answer: "answerOne",
+    incans: ["incorrect answer 1", "incorrect answer 2", "incorrect answer 3"],
+  },
+  {
+    quest: "Here is question 4?",
+    answer: "answerOne",
+    incans: ["incorrect answer 1", "incorrect answer 2", "incorrect answer 3"],
+  },
+  {
+    quest: "Here is question 5?",
+    answer: "answerOne",
     incans: ["incorrect answer 1", "incorrect answer 2", "incorrect answer 3"],
   },
 ];
 
 quizzer.addEventListener("click", startTimer);
+ansOne.addEventListener("click", askQuestion);
+ansTwo.addEventListener("click", askQuestion);
+ansThree.addEventListener("click", askQuestion);
+ansFour.addEventListener("click", askQuestion);
+
 
 var timeLeft = 60;
-console.log(quizzer);
-
+//This function starts the countdown
 function startTimer() {
-    askQuestion();
+//   askQuestion();
   var timer = setInterval(function () {
     timeLeft--;
     quizzer.textContent = timeLeft + " seconds remaining";
 
     if (timeLeft === 0) {
-      clearInterval(timeLeft);
+      clearInterval(timer);
+      window.alert("Times Up!")
     }
   }, 1000);
-}
+  question.textContent = questions[questionCount].quest;
+  ansOne.textContent = questions[questionCount].answer;
+  ansTwo.textContent = questions[questionCount].incans[0];
+  ansThree.textContent = questions[questionCount].incans[1];
+  ansFour.textContent = questions[questionCount].incans[2];
+
+};
 
 
-function askQuestion() {
-    question.textContent = questions[i].quest;
-    ansOne.textContent = questions[i].answer;
-    ansTwo.textContent = questions[i].incans[0];
-    ansThree.textContent = questions[i].incans[1];
-    ansFour.textContent = questions[i].incans[2];
+function askQuestion(event) {
+    console.log(event)
+    var userAnswer = event.target.id;
+    if (userAnswer === questions[questionCount].answer){
+        //handle correct answer
+        userScore += 1;
+        gameScore.textContent = userScore + "/" + questions.length;
+    } else {
+        gameScore.textContent = userScore + "/" + questions.length;
 
-}
+        // wrong answer
+    }
 
+   if(questionCount < questions.length){
+       questionCount += 1;
+   };
+
+    console.log(userAnswer === questions[questionCount].answer);
+    question.textContent = questions[questionCount].quest;
+  ansOne.textContent = questions[questionCount].answer;
+  ansTwo.textContent = questions[questionCount].incans[0];
+  ansThree.textContent = questions[questionCount].incans[1];
+  ansFour.textContent = questions[questionCount].incans[2];
+}; 
+   
