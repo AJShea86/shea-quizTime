a78ce38c0f87417fd2acff5eab2282e2e3eeef69
Andrew S <andrewsheamls@gmail.com>
2022-02-14 18:29:10 -0700

    created some variables in js

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 
 
+
+var question = document.getElementById("quizQuest");
+
+var ansOne = document.getElementById("answerOne");
+
+var ansTwo = document.getElementById("answerTwo");
+
+var ansThree = document.getElementById("answerThree");
+
+var ansFour = document.getElementById("answerFour");
+
+
+
+
+
