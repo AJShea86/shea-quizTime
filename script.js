842cfaf0d32e42e6d9c17022bb3059cfdec5f344
Andrew S <andrewsheamls@gmail.com>
2022-02-14 20:03:36 -0700

    created a working timer and set questions and answers

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-
-
-
 var question = document.getElementById("quizQuest");
 
 var ansOne = document.getElementById("answerOne");
@@ -11,7 +8,52 @@ var ansThree = document.getElementById("answerThree");
 
 var ansFour = document.getElementById("answerFour");
 
+var quizzer = document.getElementById("quiz");
+
+var i = 0
+
+var questions = [
+  {
+    quest: "Here is question 1?",
+    answer: "correct answer 1",
+    incans: ["incorrect answer 1", "incorrect answer 2", "incorrect answer 3"],
+  },
+  {
+    quest: "Here is question 2?",
+    answer: "correct answer 2",
+    incans: ["incorrect answer 1", "incorrect answer 2", "incorrect answer 3"],
+  },
+  {
+    quest: "Here is question 3?",
+    answer: "correct answer 3",
+    incans: ["incorrect answer 1", "incorrect answer 2", "incorrect answer 3"],
+  },
+];
+
+quizzer.addEventListener("click", startTimer);
+
+var timeLeft = 60;
+console.log(quizzer);
+
+function startTimer() {
+    askQuestion();
+  var timer = setInterval(function () {
+    timeLeft--;
+    quizzer.textContent = timeLeft + " seconds remaining";
+
+    if (timeLeft === 0) {
+      clearInterval(timeLeft);
+    }
+  }, 1000);
+}
 
 
+function askQuestion() {
+    question.textContent = questions[i].quest;
+    ansOne.textContent = questions[i].answer;
+    ansTwo.textContent = questions[i].incans[0];
+    ansThree.textContent = questions[i].incans[1];
+    ansFour.textContent = questions[i].incans[2];
 
+}
 
